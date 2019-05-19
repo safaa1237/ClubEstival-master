a1ff7080581976a54ca822c6cc84a3f4646406bb
Yasser <yassirsaadi4@gmail.com>
2019-05-19 16:19:32 +0000

    Changing main page from ReservationView to Acceuil

--- src/controllers/Accueil.java
+++ src/controllers/Accueil.java
@@ -1,13 +1,32 @@
 package controllers;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.Pane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Accueil implements Initializable {
+    @FXML
+    private JFXButton btn_m , btn_a;
+    @FXML
+    private Pane heb , main;
+
+    @FXML
+    private void btn1(ActionEvent e) {
+
+        main.toFront();
+    }
+    @FXML
+    private void btn2 (ActionEvent e) {
+        heb.toFront();
+
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
     }
 }
--- src/controllers/UserController/ReserverController.java
+++ src/controllers/UserController/ReserverController.java
@@ -1,42 +1,14 @@
 package controllers.UserController;
 
-import Utils.Constants;
-import controllers.MainViewController;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
+
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.layout.StackPane;
-import com.jfoenix.controls.JFXButton;
-import java.io.IOException;
+
 import java.net.URL;
-import java.util.ResourceBundle;
-import java.awt.*;
-import javafx.event.ActionEvent;
-
-import javafx.scene.layout.Pane;
-
 import java.util.ResourceBundle;
 
 
 public class ReserverController implements Initializable {
-        @FXML
-        private JFXButton btn_m , btn_a;
-        @FXML
-        private Pane heb , main;
 
-        @FXML
-        private void btn1(ActionEvent e) {
-
-                main.toFront();
-            }
-    @FXML
-    private void btn2 (ActionEvent e) {
-                    heb.toFront();
-
-            }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
